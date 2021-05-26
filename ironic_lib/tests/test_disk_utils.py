d682b9c43f6c79b69e164c108932e862e4403edf
Julia Kreger <juliaashleykreger@gmail.com>
2021-05-26 13:36:59 -0700

    Limit the number of malloc arenas for qemu-img convert
    Under heavy load, the qemu-img convert method may attempt to create
    additional malloc arenas to complete its work. By default this number
    is 8 * ncpu * which at about 250 MB of memory which can be consumed
    in this mannor should the.
    
    Since this is only something which should realistically occur under
    heavy cross-thread load where memory locking prevents a thread from
    unlocking a range of memory, (such as what can happen with hypervisors
    and VMs inside those hypervisors), then a new arena gets created, and
    the memory consumption spikes.
    
    More information is avialable at:
    https://www.gnu.org/software/libc/manual/html_node/Memory-Allocation-Tunables.html
    
    Change-Id: I8b2b490f2cc0ac5f47c3aaaaa249ce59db6602d6
    Story: 2008928
    Task: 42529
    (cherry picked from commit 76cbb9623f6dc855528ad0d1a614931124c6fd9c)
    (cherry picked from commit f4cabc045478826effb9ea7df0ab9e748dd66816)

--- ironic_lib/tests/test_disk_utils.py
+++ ironic_lib/tests/test_disk_utils.py
@@ -1042,11 +1042,13 @@ class OtherFunctionTestCase(base.IronicLibTestCase):
     @mock.patch.object(utils, 'execute', autospec=True)
     def test_convert_image(self, execute_mock):
         disk_utils.convert_image('source', 'dest', 'out_format')
-        execute_mock.assert_called_once_with('qemu-img', 'convert', '-O',
-                                             'out_format', 'source', 'dest',
-                                             run_as_root=False,
-                                             prlimit=mock.ANY,
-                                             use_standard_locale=True)
+        execute_mock.assert_called_once_with(
+            'qemu-img', 'convert', '-O',
+            'out_format', 'source', 'dest',
+            run_as_root=False,
+            prlimit=mock.ANY,
+            use_standard_locale=True,
+            env_variables={'MALLOC_ARENA_MAX': '3'})
 
     @mock.patch.object(utils, 'execute', autospec=True)
     def test_convert_image_retries(self, execute_mock):
@@ -1064,7 +1066,8 @@ class OtherFunctionTestCase(base.IronicLibTestCase):
                                  'out_format', 'source', 'dest',
                                  run_as_root=False,
                                  prlimit=mock.ANY,
-                                 use_standard_locale=True)
+                                 use_standard_locale=True,
+                                 env_variables={'MALLOC_ARENA_MAX': '3'})
         execute_mock.assert_has_calls([
             convert_call,
             mock.call('sync'),
@@ -1091,7 +1094,8 @@ class OtherFunctionTestCase(base.IronicLibTestCase):
                                  'out_format', 'source', 'dest',
                                  run_as_root=False,
                                  prlimit=mock.ANY,
-                                 use_standard_locale=True)
+                                 use_standard_locale=True,
+                                 env_variables={'MALLOC_ARENA_MAX': '3'})
         execute_mock.assert_has_calls([
             convert_call,
             mock.call('sync'),
@@ -1114,7 +1118,8 @@ class OtherFunctionTestCase(base.IronicLibTestCase):
                                  'out_format', 'source', 'dest',
                                  run_as_root=False,
                                  prlimit=mock.ANY,
-                                 use_standard_locale=True)
+                                 use_standard_locale=True,
+                                 env_variables={'MALLOC_ARENA_MAX': '3'})
         execute_mock.assert_has_calls([
             convert_call,
             mock.call('sync'),
